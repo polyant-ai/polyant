c6d58e81c619cd9112fb7f8748245b17df8a785a
Fabrizio Regini <fabrizio@exelab.com>
2026-06-10 01:20:54 +0200

    feat(embeddings): opt-in re-embed job + POST /api/instances/:slug/memories/re-embed
    Signed-off-by: Fabrizio Regini <fabrizio@exelab.com>

--- packages/engine/src/server/server.module.ts
+++ packages/engine/src/server/server.module.ts
@@ -7,6 +7,7 @@ import { OpenAIModule } from "./openai/openai.module.js";
 import { AuthModule } from "../auth/auth.module.js";
 import { HealthController } from "./health/health.controller.js";
 import { MemoriesController } from "./memories/memories.controller.js";
+import { ReEmbedController } from "./memories/re-embed.controller.js";
 import { InstancesController } from "./instances/instances.controller.js";
 import { InstancePromptsController } from "./instances/instance-prompts.controller.js";
 import { InstanceToolsController } from "./instances/instance-tools.controller.js";
@@ -49,6 +50,7 @@ import { ActivityStreamModule } from "../activity-stream/activity-stream.module.
   controllers: [
     HealthController,
     MemoriesController,
+    ReEmbedController,
     InstancesController,
     InstancePromptsController,
     InstanceToolsController,
